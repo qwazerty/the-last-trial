c2f06c8bd4fe81ea6081671fe175c16529abf9f1
qwazerty <precursor.qwaz@hotmail.fr>
2010-12-15 14:57:15 +0000

    l objet le plus important du jeu LA BOITE, et suppression des sons

--- The Last Trial/The Last Trial/Game1.cs
+++ The Last Trial/The Last Trial/Game1.cs
@@ -51,7 +51,7 @@ namespace The_Last_Trial
             map1.S_Texture(Content.Load<Texture2D>("map/1"));
             map_first.S_Texture(Content.Load<Texture2D>("map/2"));
 
-            backsound.Load(Content.Load<Song>("music/Kalimba"));
+            // backsound.Load(Content.Load<Song>("music/Kalimba"));
 
             map1.S_Position(new Vector2(0, 0));
             map_first.S_Position(new Vector2(0, 737));
